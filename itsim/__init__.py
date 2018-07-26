6d5c6a829d811693507b0b014e2949dcbbdc3d1d
Benoit Hamelin <benoit@benoithamelin.com>
2018-07-26 11:42:39 -0400

    Impl IP address description

--- itsim/__init__.py
+++ itsim/__init__.py
@@ -1,2 +1,15 @@
-def hello_world():
-    return "hello"
+from ipaddress import IPv4Address, IPv6Address, ip_address
+from typing import Union
+
+
+Address = Union[IPv4Address, IPv6Address]
+Addressable = Union[None, str, int, Address]
+Port = int
+
+
+def as_address(aa: Addressable) -> Address:
+    if aa is None:
+        return ip_address(0)
+    elif isinstance(aa, int) or isinstance(aa, str):
+        return ip_address(aa)
+    return aa
--- tests/test_itsim.py
+++ tests/test_itsim.py
@@ -1,5 +1,22 @@
-from itsim import hello_world
+from ipaddress import ip_address
+
+from itsim import as_address
 
 
-def test_hello_world():
-    assert hello_world() == "hello"
+def test_none_as_address():
+    assert as_address(None) == ip_address(0)
+
+
+def test_int_as_address():
+    assert as_address(0) == ip_address(0)
+    assert as_address(0xc0a80438) == ip_address("192.168.4.56")
+
+
+def test_str_as_address():
+    assert as_address("0.0.0.0") == ip_address(0)
+    assert as_address("192.168.4.56") == ip_address("192.168.4.56")
+
+
+def test_address_as_address():
+    assert as_address(ip_address(0)) == ip_address(0)
+    assert as_address(ip_address("192.168.4.56")) == ip_address("192.168.4.56")
