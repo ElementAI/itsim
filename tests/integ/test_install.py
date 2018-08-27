b5375c30540fdbc15af81abd8bd0dcec5c137aa0
Matt <matt.craddock@elementai.com>
2018-08-27 19:17:05 -0400

    Clean up tests as per review comments

--- tests/integ/test_install.py
+++ tests/integ/test_install.py
@@ -5,9 +5,11 @@ from greensim.tags import Tags
 from itsim.it_objects.endpoint import Endpoint
 from itsim.network import Network
 
-############################################################
-# Define constants for the many tests of install(_in, _at) #
-############################################################
+from pytest import fixture
+
+#########################################################################
+# Define constants and fixtures for the many tests of install(_in, _at) #
+#########################################################################
 
 
 class IntegTestTag(Tags):
@@ -16,14 +18,28 @@ class IntegTestTag(Tags):
     CANADA = {}
 
 
-flag = 0
-sim = Simulator()
-net = Network(sim,
-              cidr="192.168.4.0/24",
-              bandwidth=constant(1),
-              latency=normal(5, 1),
-              num_skip_addresses=100)
-traveller = Endpoint("Ishmael", net)
+ENDPOINT_PROPERTY = "test_calling_endpoint"
+FLAG_PROPERTY = "test_flag"
+SHORT_TAG_SET = set([IntegTestTag.ALICE, IntegTestTag.BOB])
+
+
+@fixture
+def simulator():
+    return Simulator()
+
+
+@fixture
+def network(simulator):
+    return Network(simulator,
+                   cidr="192.168.4.0/24",
+                   bandwidth=constant(1),
+                   latency=normal(5, 1),
+                   num_skip_addresses=100)
+
+
+@fixture
+def endpoint(network):
+    return Endpoint("Traveller", network)
 
 
 ####################################################################################
@@ -48,60 +64,61 @@ def create_time_check(delay, fn):
 
 
 # Vanilla install, just passes arguments through and checks that fn is run using flag
-def run_install(sim, node, fn, *args, **kwargs):
-    global flag
-    flag = 0
-    sim._clear()
-    node.install(fn, *args, **kwargs)
+def run_install(sim, end, fn, *args, **kwargs):
+    setattr(sim, ENDPOINT_PROPERTY, end)
+    setattr(sim, FLAG_PROPERTY, 0)
+    end.install(fn, *args, **kwargs)
     sim.run()
-    assert flag == 1
+    assert 1 == getattr(sim, FLAG_PROPERTY)
 
 
-# Wraps fn in another function that checks the delay happened and passes arguments and tags through
-def run_install_at(sim, node, fn, *args, **kwargs):
-    global flag
-    flag = 0
+# Wraps fn in another function that checks the delay happend and passes arguments through
+def run_install_at(sim, end, fn, *args, **kwargs):
     delay = 10
 
-    sim._clear()
-    node.install_at(delay, create_time_check(delay, fn), *args, **kwargs)
+    def time_check(*args, **kwargs):
+        assert delay == now()
+        fn(*args, **kwargs)
+
+    setattr(sim, ENDPOINT_PROPERTY, end)
+    setattr(sim, FLAG_PROPERTY, 0)
+    end.install_at(delay, create_time_check(delay, fn), *args, **kwargs)
     sim.run()
-    assert flag == 1
+    assert 1 == getattr(sim, FLAG_PROPERTY)
 
 
 # Same as above, but runs the simulation first to make sure the relative functionality of _in is used
-def run_install_in(sim, node, fn, *args, **kwargs):
-    global flag
-    flag = 0
+def run_install_in(sim, end, fn, *args, **kwargs):
     delay = 10
 
-    sim._clear()
+    def time_check(*args, **kwargs):
+        assert 2 * delay == now()
+        fn(*args, **kwargs)
+
+    setattr(sim, ENDPOINT_PROPERTY, end)
+    setattr(sim, FLAG_PROPERTY, 0)
     sim.run(delay)
-    node.install_in(delay, create_time_check(2 * delay, fn), *args, **kwargs)
+    end.install_in(delay, create_time_check(2 * delay, fn), *args, **kwargs)
     sim.run()
-    assert flag == 1
+    assert 1 == getattr(sim, FLAG_PROPERTY)
 
 
-short_tag_set = set([IntegTestTag.ALICE, IntegTestTag.BOB])
-
-
-@tagged(*short_tag_set)
+@tagged(*SHORT_TAG_SET)
 def tag_checker(node, tag_set):
-    global flag
-    flag = 1
+    setattr(Process.current().rsim(), FLAG_PROPERTY, 1)
     assert tag_set == Process.current()._tag_set
 
 
-def test_install_tags():
-    run_install(sim, traveller, tag_checker, short_tag_set)
+def test_install_tags(endpoint):
+    run_install(endpoint.sim, endpoint, tag_checker, SHORT_TAG_SET)
 
 
-def test_install_at_tags():
-    run_install_at(sim, traveller, tag_checker, short_tag_set)
+def test_install_at_tags(endpoint):
+    run_install_at(endpoint.sim, endpoint, tag_checker, SHORT_TAG_SET)
 
 
-def test_install_in_tags():
-    run_install_in(sim, traveller, tag_checker, short_tag_set)
+def test_install_in_tags(endpoint):
+    run_install_in(endpoint.sim, endpoint, tag_checker, SHORT_TAG_SET)
 
 
 # As defined in the __init__ method of Process, a new Process should take tags from
@@ -109,28 +126,28 @@ def test_install_in_tags():
 # These test that install shows consistent behavior
 
 
-def test_install_propogate():
+def test_install_propogate(endpoint):
 
     @tagged(IntegTestTag.CANADA)
     def tag_propogator(node, tag_set):
         node.install(tag_checker, tag_set | set([IntegTestTag.CANADA]))
 
-    run_install(sim, traveller, tag_propogator, short_tag_set)
+    run_install(endpoint.sim, endpoint, tag_propogator, SHORT_TAG_SET)
 
 
-def test_install_at_propogate():
+def test_install_at_propogate(endpoint):
 
     @tagged(IntegTestTag.CANADA)
     def tag_propogator(node, tag_set):
         node.install_at(now() + 10, create_time_check(now() + 10, tag_checker), tag_set | set([IntegTestTag.CANADA]))
 
-    run_install_at(sim, traveller, tag_propogator, short_tag_set)
+    run_install_at(endpoint.sim, endpoint, tag_propogator, SHORT_TAG_SET)
 
 
-def test_install_in_propogate():
+def test_install_in_propogate(endpoint):
 
     @tagged(IntegTestTag.CANADA)
     def tag_propogator(node, tag_set):
         node.install_in(10, create_time_check(now() + 10, tag_checker), tag_set | set([IntegTestTag.CANADA]))
 
-    run_install_in(sim, traveller, tag_propogator, short_tag_set)
+    run_install_in(endpoint.sim, endpoint, tag_propogator, SHORT_TAG_SET)
