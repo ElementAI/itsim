32bc58c811b2439fb2e30d01ba41c936ae3a9a34
Matt <matt.craddock@elementai.com>
2018-10-22 17:42:51 -0400

    Sphinx cleanup

--- itsim/internet.py
+++ itsim/internet.py
@@ -1,10 +1,9 @@
 from typing import Optional, Callable
 
-from itsim.it_objects import ITObject
+from itsim.it_objects import ITObject, Simulator
 from itsim.link import Link
 from itsim.node import Host
 from itsim.random import VarRandomSize, VarRandomTime, VarRandomBandwidth
-from itsim.simulator import Simulator
 from itsim.types import HostnameRepr, Protocol, PortsRepr
 
 
@@ -39,17 +38,17 @@ class InternetHost(Host):
         Makes the node run a web server over common HTTP ports, in a request/reply paradigm (one logical request packet,
         one logical reply packet).
 
-        - :param sim: Simulator instance.
-        - :param len_response: Random number sequence corresponding to the length in bytes of the response to each
-          request.
-        - :param protocol: Protocols over which the service is provided. Value `Protocol.CLEAR` indicates cleartext HTTP
-          service over port 80; value `Protocol.SSL` indicates crypted HTTPS service over port 443. By default, both are
-          provided.
-        - :param frequency: Relative frequency of requests sent over each serviced port, assuming other types of service
-          besides one-shot request/reply embodied by this service. For instance, if streaming service is also provided
-          over HTTPS, with a frequency 2, while frequency 1 is selected for the request/reply server, then requests to
-          port 443 are will be randomly sampled so that they are considered to be streaming requests (and deserved
-          accordingly) twice as much as request/reply requests.
+        -:param sim: Simulator instance.
+        -:param len_response: Random number sequence corresponding to the length in bytes of the response to each
+        request.
+        -:param protocol: Protocols over which the service is provided. Value `Protocol.CLEAR` indicates cleartext HTTP
+        service over port 80; value `Protocol.SSL` indicates crypted HTTPS service over port 443. By default, both are
+        provided.
+        -:param frequency: Relative frequency of requests sent over each serviced port, assuming other types of service
+        besides one-shot request/reply embodied by this service. For instance, if streaming service is also provided
+        over HTTPS, with a frequency 2, while frequency 1 is selected for the request/reply server, then requests to
+        port 443 are will be randomly sampled so that they are considered to be streaming requests (and deserved
+        accordingly) twice as much as request/reply requests.
         """
         raise NotImplementedError()
         return self
@@ -66,18 +65,18 @@ class InternetHost(Host):
         Makes the node run a web server over common HTTP ports, in a streaming paradigm (one logical request packet,
         followed by a sequence of logical reply packets).
 
-        - :param sim: Simulator instance.
-        - :param bandwidth_usage: While replying, the server will generate packets so as to use bandwidth in accordance
-          with numbers gotten from this random number generator.
-        - :param duration: Duration of the stream reply is taken from this random number generator.
-        - :param protocol: Protocols over which the service is provided. Value `Protocol.CLEAR` indicates cleartext HTTP
-          service over port 80; value `Protocol.SSL` indicates crypted HTTPS service over port 443. By default,
-          streaming is provided only over HTTPS.
-        - :param frequency: Relative frequency of requests sent over each serviced port, assuming other types of service
-          besides one-shot request/reply embodied by this service. For instance, if streaming service is also provided
-          over HTTPS, with a frequency 2, while frequency 1 is selected for the request/reply server, then requests to
-          port 443 are will be randomly sampled so that they are considered to be streaming requests (and deserved
-          accordingly) twice as much as request/reply requests.
+        -:param sim: Simulator instance.
+        -:param bandwidth_usage: While replying, the server will generate packets so as to use bandwidth in accordance
+        with numbers gotten from this random number generator.
+        -:param duration: Duration of the stream reply is taken from this random number generator.
+        -:param protocol: Protocols over which the service is provided. Value `Protocol.CLEAR` indicates cleartext HTTP
+        service over port 80; value `Protocol.SSL` indicates crypted HTTPS service over port 443. By default,
+        streaming is provided only over HTTPS.
+        -:param frequency: Relative frequency of requests sent over each serviced port, assuming other types of service
+        besides one-shot request/reply embodied by this service. For instance, if streaming service is also provided
+        over HTTPS, with a frequency 2, while frequency 1 is selected for the request/reply server, then requests to
+        port 443 are will be randomly sampled so that they are considered to be streaming requests (and deserved
+        accordingly) twice as much as request/reply requests.
         """
         raise NotImplementedError()
         return self
@@ -95,14 +94,14 @@ class InternetHost(Host):
         paradigm is that the client periodically sends new *request* packets over the same socket connection, which get
         immediate 1-packet replies; in addition, the server sends unsollicited *update* packets over the connection.
 
-        - :param sim: Simulator instance.
-        - :param duration: Duration of the websocket session is taken from this random number generator. After that, the
-          connection with the client is closed.
-        - :param request_interval: Interval suggested to the client to wait between sending websocket requests, as a
-          number generator.
-        - :param update_interval: The wait times between updates are taken from this random generator.
-        - :param len_beacon: The length of each sent packet (whether as a reply to a request or as an update) is taken
-          from this random generator.
+        -:param sim: Simulator instance.
+        -:param duration: Duration of the websocket session is taken from this random number generator. After that, the
+        connection with the client is closed.
+        -:param request_interval: Interval suggested to the client to wait between sending websocket requests, as a
+        number generator.
+        -:param update_interval: The wait times between updates are taken from this random generator.
+        -:param len_beacon: The length of each sent packet (whether as a reply to a request or as an update) is taken
+        from this random generator.
         """
         raise NotImplementedError()
         return self
@@ -119,12 +118,12 @@ class InternetHost(Host):
         Makes the node run a secure shell server (SSH). The service paradigm is a sequence of requests and responses,
         over a single connection. The service is provided over port 22.
 
-        - :param sim: Simulator instance.
-        - :param duration: Duration of the shell session is taken from this random generator.
-        - :param interval: Time interval between requests, which is suggested to the client, is taken from this random
-          number generator.
-        - :param request: Length (in bytes) of requests is taken from this random generator.
-        - :param response: Length (in bytes) of responses is taken from this random generator.
+        -:param sim: Simulator instance.
+        -:param duration: Duration of the shell session is taken from this random generator.
+        -:param interval: Time interval between requests, which is suggested to the client, is taken from this random
+        number generator.
+        -:param request: Length (in bytes) of requests is taken from this random generator.
+        -:param response: Length (in bytes) of responses is taken from this random generator.
         """
         raise NotImplementedError()
         return self
