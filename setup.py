51acc302884551652ec5f02785b637868002a3f9
Matt <matt.craddock@elementai.com>
2018-10-23 16:49:40 -0400

    Initial RTD prep with setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,20 @@
 from setuptools import setup
-# from subprocess import check_output
+from setuptools.command.install import install
+from subprocess import check_call
 
 with open("README.md", "r") as file_long_description:
     long_description = file_long_description.read()
+
+sphinx_cmd_list = ["sphinx-apidoc itsim -o sphinx/source/modules",
+                   "make -C sphinx html"]
+
+# sphinx_cmd = "(%s)" % " && ".join(sphinx_cmd_list)
+
+class PostInstall(install):
+    def run(self) -> None:
+        for cmd in sphinx_cmd_list:
+            print(cmd.split(" "))
+            check_call(cmd.split(" "))
 
 setup(
     name='itsim',
@@ -18,5 +30,8 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent"
-    )
+    ),
+    cmdclass = {"install": PostInstall}
 )
+
+
