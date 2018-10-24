307177ad35632399a211acee65f457fb9d7bc360
Benoit Hamelin <benoit@benoithamelin.com>
2018-10-24 17:14:42 -0400

    Remove sim parameter to Endpoint constructor

--- itsim/endpoint.py
+++ itsim/endpoint.py
@@ -5,9 +5,7 @@ from itsim.node import Node
 class Endpoint(Node):
     """
     General-purpose computer, on which we have complete behaviour visibility through agent telemetry.
-
-    :param sim: Simulator instance to which this endpoint participates.
     """
 
-    def __init__(self, sim: Simulator) -> None:
+    def __init__(self) -> None:
         raise NotImplementedError()
