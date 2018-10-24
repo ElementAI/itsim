4a19a5368f129b8a4d8ae79b24d672f1d2a12c80
Benoit Hamelin <benoit@benoithamelin.com>
2018-10-24 16:49:33 -0400

    Add docstring to Router class

--- itsim/node/router.py
+++ itsim/node/router.py
@@ -3,6 +3,14 @@ from itsim.node import Node
 
 
 class Router(Node):
+    """
+    Node tasked with forwarding messages between LANs connected to it, and over to a WAN interface. The router is
+    configured through links it forwards between, and thus is made to implement certain network services over the
+    appropriate links.
+
+    :param wan: WAN interface, articulated by a ``itsim.network.Link`` instance.
+    :param lan: LAN links connected to this router.
+    """
 
     def __init__(self, wan: Link, *lan: Link) -> None:
         super().__init__()
