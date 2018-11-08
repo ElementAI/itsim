9bd1db1e5053c9c8e528377455a4b1ad1552c0ea
Benoit Hamelin <benoit@benoithamelin.com>
2018-11-08 17:48:48 -0500

    Finalize merge of new networking into node.py

--- itsim/machine/node.py
+++ itsim/machine/node.py
@@ -1,18 +1,15 @@
 from collections import OrderedDict
-from contextlib import contextmanager
-from ipaddress import _BaseAddress
 from queue import Queue
-from typing import Callable, cast, Generator, Iterable, MutableMapping, Optional, Set, Tuple, TypeVar
+from typing import Callable, Generator, MutableMapping, Optional, Set, Tuple, TypeVar, Iterator
 
 import greensim
 
-from ipaddress import ip_address
-
 from itsim import _Node
 from itsim import ITObject
-from itsim.network import _Link
 from itsim.network.connection import Connection
-from itsim.network.location import AddressInUse, InvalidAddress, Location
+from itsim.network.interface import Interface
+from itsim.network.link import Link, Loopback
+from itsim.network.location import Location, LocationRepr
 from itsim.network.packet import Payload, Packet
 from itsim.machine.file_system import File
 from itsim.machine.process_management import _Daemon
@@ -20,54 +17,97 @@ from itsim.machine.process_management.process import Process
 from itsim.machine.process_management.thread import Thread
 from itsim.machine.user_management import UserAccount
 from itsim.simulator import Simulator
-from itsim.types import Address, AddressRepr, as_port, Port, PortRepr, Protocol
+from itsim.types import Address, AddressRepr, Port, PortRepr, Hostname, as_address, Cidr, as_port
 
 
 MapPorts = MutableMapping[Port, Process]
 
 
+class _DestinationError(Exception):
+
+    def __init__(self, dest: Address) -> None:
+        super().__init__()
+        self.dest = dest
+
+
+class NotNeighbouring(_DestinationError):
+    pass
+
+
+class NoSuitableSourceAddress(_DestinationError):
+    pass
+
+
 class Socket(ITObject):
 
-    def __init__(self, src: Location, node: _Node) -> None:
+    def __init__(self, port: Port, node: _Node) -> None:
         super().__init__()
-        self._src = src
+        self._is_closed = False
+        self._port = port
         self._node = node
         self._packet_queue: Queue[Packet] = Queue()
-        self._packet_signal: greensim.Signal = greensim.Signal()
-        self._packet_signal.turn_off()
+        self._packet_signal: greensim.Signal = greensim.Signal().turn_off()
 
-    def send(self, dest: Location, byte_size: int, payload: Payload) -> None:
-        ob_packet = Packet(self._src, dest, byte_size, payload)
-        self._node._send_to_network(ob_packet)
+    @property
+    def port(self):
+        return self._port
 
-    # Placeholder method
-    def broadcast(self, port: int, byte_size: int, payload: Payload) -> None:
-        """
-        This method is currently a placeholder under active development
-        """
-        pass
+    def __enter__(self):
+        yield self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> bool:
+        self.close()
+        return False
+
+    def close(self) -> None:
+        self._node._close_socket(self.port)
+        self._is_closed = True
+
+    def send(self, dr: LocationRepr, size: int, payload: Optional[Payload] = None) -> None:
+        if self._is_closed:
+            raise ValueError("Socket is closed")
+        dest = Location.from_repr(dr)
+        address_dest = self._resolve_destination_final(dest.hostname)
+        address_src = self._find_address_source(address_dest)
+        self._node._send_packet(
+            Packet(Location(address_src, self.port), Location(address_dest, dest.port), size, payload)
+        )
+
+    def _resolve_destination_final(self, hostname_dest: Hostname) -> Address:
+        try:
+            return as_address(hostname_dest)
+        except ValueError:
+            return self._node.resolve_name(hostname_dest)
+
+    def _find_address_source(self, address_dest: Address) -> Address:
+        try:
+            return self._node.get_address_neighbour(address_dest)
+        except NotNeighbouring:
+            for address in self._node.iter_addresses_with_gateway(address_dest):
+                return address
+        raise NoSuitableSourceAddress(address_dest)
 
     def _enqueue(self, packet: Packet) -> None:
         self._packet_queue.put(packet)
         self._packet_signal.turn_on()
 
     def recv(self) -> Packet:
-        # Waiting loop
-        while self._packet_queue.empty():
-            self._packet_signal.wait()
+        if self._is_closed:
+            raise ValueError("Socket is closed")
 
-        # Make sure to update the Signal in case more Processes are in the Signal's queue
+        self._packet_signal.wait()
         output = self._packet_queue.get()
-
         if self._packet_queue.empty():
             self._packet_signal.turn_off()
+
         return output
 
 
-class Host(object):
+class PortAlreadyInUse(Exception):
 
-    def __init__(self):
-        raise NotImplementedError()
+    def __init__(self, port: Port) -> None:
+        super().__init__()
+        self.port = port
 
 
 class Node(_Node):
@@ -76,120 +116,83 @@ class Node(_Node):
 
     def __init__(self):
         super().__init__()
-        self._address_default: Optional[Address] = None
-        self._sockets: MutableMapping[Location, Socket] = OrderedDict()
-        self._links: MutableMapping[AddressRepr, _Link] = OrderedDict()
+        self._interface_default = None
+        self._interfaces: MutableMapping[Cidr, Interface] = OrderedDict()
+        self.connected_to(Loopback(), "127.0.0.1")
+        self._sockets: MutableMapping[Port, Socket] = OrderedDict()
+
         self._proc_set: Set[Process] = set()
         self._process_counter: int = 0
         self._default_process_parent = Process(-1, self)
         self._port_table: MutableMapping[Port, Connection] = OrderedDict()
 
-    def connected_to(self, link: _Link) -> "Node":
+    def connected_to(self, link: Link, ar: AddressRepr = None, is_default: bool = True) -> "Node":
         """
         Configures a budding node to be connected to a given link.
 
         :return: The node instance, so it can be further built.
         """
-        raise NotImplementedError()
+        link._connect(self)
+        interface = Interface(link, ar)
+        self._interfaces[link.cidr] = interface
+        if is_default:
+            self._interface_default = interface
+
+        # TODO -- Decide whether to set up DHCP client for this interface
         return self
 
-    def add_physical_link(self, link: _Link, ar: AddressRepr) -> None:
-        """
-        Attempt to connect this Node to the given Link at the given AddressRepr.
-        If the AddressRepr is already being used to point to a Link, this will throw AddressInUse.
-        Otherwise, it will call add_node on the Link (which in turn will call as_address from itsim.types
-        on the AddressRepr) and if the call succeeds this method will store a reference
-        to the Link internally at the AddressRepr
-        """
-
-        if ar in self._links:
-            raise AddressInUse(ar)
-
-        link.add_node(self, ar)
-
-        self._links[ar] = link
-
-    def remove_physical_link(self, ar: AddressRepr) -> bool:
-        """
-        Attempt to drop the Link that is connected at the given AddressRepr.
-        If there is no known Link at the AddressRepr, this method will return False.
-        Otherwise, it will free up the AddressRepr for another Node and call drop_node
-        on the Link that was previously stored there, returning its result
-        """
-
-        if ar not in self._links:
-            return False
-
-        link = self._links[ar]
-        del self._links[ar]
-        return link.drop_node(ar)
-
-    @property
-    def addresses(self) -> Iterable[Address]:
+    def addresses(self) -> Iterator[Address]:
         """
         This method is currently a placeholder under active development
         """
-        return []
+        return (interface.address for interface in self.interfaces())
+
+    def interfaces(self) -> Iterator[Interface]:
+        yield from self._interfaces.values()
 
     @property
     def address_default(self) -> Address:
         """
         This method is currently a placeholder under active development
         """
-        return ip_address('127.0.0.1')
+        return self._interface_default.address
 
-    def _as_location(self, lb: Optional[LocationBind] = None) -> Location:
-        if lb is None:
-            return Location(self.address_default, 0)
-        elif isinstance(lb, int):
-            return Location(self.address_default, cast(Port, lb))
-        elif isinstance(lb, (str, _BaseAddress)):
-            return Location(cast(AddressRepr, lb), 0)
-        elif isinstance(lb, tuple):
-            return Location(lb[0], lb[1])
-        elif isinstance(lb, Location):
-            return cast(Location, lb)
-        raise ValueError("What is that LocationBind instance?")
-
-    def _as_source_bind(self, lb: Optional[LocationBind] = None) -> Location:
-        loc = self._as_location(lb)  # type: ignore
-
-        # Address here must be one of the node's addresses.
-        if not isinstance(loc.hostname, _BaseAddress) or loc.hostname not in self.addresses:
-            raise InvalidAddress(loc.hostname)
-        elif loc.hostname not in self.addresses:
-            raise InvalidAddress(loc.hostname)
-        else:
-            address = loc.hostname
-
-        port: Port = loc.port
-
-        return Location(address, port)
-
-    # Placeholder method
-    @contextmanager
-    def bind(self, lb: Optional[LocationBind] = None) -> Generator[Location, None, None]:
+    def get_address_neighbour(self, neighbour: Address) -> Address:
         """
-        This method is currently a placeholder under active development
+        Gives the first address this node bears that is part of the same network as the given address.
         """
-        try:
-            yield self._as_source_bind(lb)  # type: ignore
-        finally:
-            # Placeholder for cleanup
-            pass
+        for interface in self.interfaces():
+            if neighbour in interface.cidr:
+                return interface.address
+        raise NotNeighbouring(neighbour)
 
-    # Placeholder method
-    @contextmanager
-    def open_socket(self, lb: Optional[LocationBind] = None) -> Generator[Socket, None, None]:
-        """
-        This method is currently a placeholder under active development
-        """
-        with self.bind(lb) as src:  # type: ignore
-            try:
-                yield Socket(src, self)
-            finally:
-                # Placeholder for cleanup
-                pass
+    def iter_addresses_with_gateway(self, address_dest: Address) -> Generator[Address, None, None]:
+        for interface in self.interfaces():
+            if interface.has_gateway:
+                yield interface.address
+
+    def _sample_port_unprivileged_free(self) -> Port:
+        raise NotImplementedError()
+
+    def bind(self, pr: PortRepr = 0) -> Socket:
+        port = as_port(pr) or self._sample_port_unprivileged_free()
+        if port in self._sockets:
+            raise PortAlreadyInUse(port)
+        socket = Socket(port, self)
+        self._sockets[port] = socket
+        return socket
+
+    def _close_socket(self, port: Port) -> None:
+        del self._sockets[port]
+
+    def _send_packet(self, packet: Packet) -> None:
+        raise NotImplementedError()
+
+    def _receive_packet(self, packet: Packet) -> None:
+        raise NotImplementedError()
+
+    def resolve_name(self, hostname: Hostname) -> Address:
+        raise NotImplementedError()
 
     def procs(self) -> Set[Process]:
         return self._proc_set
