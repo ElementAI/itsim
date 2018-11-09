e5967f10b480e9afac7cb535e958aa20b726bfc0
Benoit Hamelin <benoit@benoithamelin.com>
2018-11-09 14:36:00 -0500

    Add forwarding objects

---
+++ itsim/network/forwarding.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from itsim.network.packet import Packet
+from itsim.types import Address, as_cidr, Cidr, CidrRepr, AddressRepr, as_address
+
+
+class Forwarding(ABC):
+
+    def __init__(self, cr: Optional[CidrRepr] = None) -> None:
+        super().__init__()
+        self._cidr = as_cidr(cr or "0.0.0.0/0")
+
+    @property
+    def cidr(self) -> Cidr:
+        return self._cidr
+
+    @abstractmethod
+    def get_hop(self, packet: Packet) -> Address:
+        pass
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, type(self)):
+            return False
+        return self.cidr == other.cidr
+
+
+class Local(Forwarding):
+
+    def get_hop(self, packet: Packet) -> Address:
+        return packet.dest.hostname_as_address()
+
+
+class Relay(Forwarding):
+
+    def __init__(self, gr: AddressRepr, cr: Optional[CidrRepr] = None) -> None:
+        super().__init__(cr)
+        self._gateway = as_address(gr)
+
+    def get_hop(self, packet: Packet) -> Address:
+        return self._gateway
+
+    def __eq__(self, other: object) -> bool:
+        if not Forwarding.__eq__(self, other):
+            return False
+        return self._gateway == other._gateway
---
+++ tests/test_forwarding.py
@@ -0,0 +1,20 @@
+import pytest
+
+from itsim.network.forwarding import Local, Relay
+from itsim.network.location import Location
+from itsim.network.packet import Packet
+from itsim.types import AddressRepr, as_address
+
+
+def packet(address_dest: AddressRepr) -> Packet:
+    return Packet(Location("192.168.1.45", 9887), Location(address_dest, 443), 1080)
+
+
+def test_local():
+    f = Local("192.168.1.0/24")
+    assert f.get_hop(packet("192.168.1.89")) == as_address("192.168.1.89")
+
+
+def test_relay():
+    f = Relay("192.168.1.2", "0.0.0.0/0")
+    assert f.get_hop(packet("2.3.89.2")) == as_address("192.168.1.2")
