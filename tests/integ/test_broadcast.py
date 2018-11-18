347ca3bd4573242f816c8688763f0a627f41de70
Benoit Hamelin <benoit@benoithamelin.com>
2018-11-17 21:30:31 -0500

    Fix assert bug with integration test

--- tests/integ/test_broadcast.py
+++ tests/integ/test_broadcast.py
@@ -5,12 +5,12 @@ from greensim.random import uniform, constant
 from itsim.machine.endpoint import Endpoint
 from itsim.machine.process_management.thread import Thread
 from itsim.network.link import Link
-from itsim.simulator import Simulator, advance
+from itsim.simulator import Simulator, advance, now
 from itsim.types import Address, as_address
 from itsim.units import MS, S, MbPS
 
 
-NUMS_HOST = list(range(100, 110))
+NUMS_HOST = list(range(100, 120))
 
 
 class EndpointChattering(Endpoint):
@@ -44,8 +44,9 @@ def test_broadcast():
     sim = Simulator()
     link = Link("10.11.0.0/16", uniform(100 * MS, 200 * MS), constant(100 * MbPS))
     endpoints = [EndpointChattering(sim).connected_to(link, as_address(n, link.cidr)) for n in NUMS_HOST]
+    all_addresses = set(as_address(n, link.cidr) for n in NUMS_HOST)
 
     sim.run(10 * S)
 
     for endpoint in endpoints:
-        endpoint.assert_seen_all_peers()
+        endpoint.assert_seen_all_peers(all_addresses)
