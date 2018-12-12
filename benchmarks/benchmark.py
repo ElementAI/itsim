d09258db75daeed351d7f829485d7b4117b87958
Pierre-Luc Beaubien <pierre-luc.beaubien@elementai.com>
2018-12-11 21:33:56 -0500

    Charts with complete data for 3 fcts

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -3,6 +3,7 @@
 # https://medium.freecodecamp.org/how-to-get-embarrassingly-fast-random-subset-sampling-with-python-da9b27d494d9
 
 import time
+from statistics import mean, variance
 
 class ContextTimer():
     def __init__(self, fct):
@@ -17,33 +18,29 @@ class ContextTimer():
         self._runtime = end - self._start
         print(f'The function {self._fct} took:\n{self._runtime} s')
 
-# @benchmarker
-def benchmarker(func):
-    """
-    A timer decorator
-    """
-    def function_timer(*args, **kwargs):
+# ex: @benchmarker(100)
+def benchmarker(iterations):
+    def real_benchmarker(func):
         """
-        A nested function for timing other functions
+        A timer decorator
         """
-        nb_iterations = 100
-        results = []
-        time_avg = 0
+        def function_timer(*args, **kwargs):
+            """
+            A nested function for timing other functions
+            """
+            results = []
+            for i in range(iterations):
+                start = time.time()
+                value = func(*args, **kwargs)
+                results.append(time.time() - start)
 
-        for i in range(nb_iterations):
-            start = time.time()
-            value = func(*args, **kwargs)
-            end = time.time()
-            runtime = end - start
+            stat = {"mean": mean(results),
+                    "variance": variance(results),
+                    "min": min(results),
+                    "max": max(results)}
 
-            time_avg = runtime + time_avg
-            results.append((time_avg, value))
-
-        time_avg = time_avg/nb_iterations
-        runtime_msg = f"{func.__name__}\t avg:{time_avg} s"
-        print(runtime_msg)
-        return value, time_avg
-    return function_timer
-
+            return value, stat
+        return function_timer
+    return real_benchmarker
 
 
