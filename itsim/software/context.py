d38b418541b0d870e35bd0012f51b531a021d646
Benoit Hamelin <benoit@benoithamelin.com>
2018-12-17 15:47:35 -0500

    Fix PEP-8 compliance

--- itsim/software/context.py
+++ itsim/software/context.py
@@ -1,10 +1,6 @@
-from typing import Callable, Any, Generator
-
 from itsim import ITObject
-from itsim.machine import _Socket, _Node
+from itsim.machine import _Node
 from itsim.machine.process_management import _Process, _Thread
-from itsim.simulator import Simulator
-from itsim.types import Protocol, PortRepr, Address
 
 
 class Context(ITObject):
