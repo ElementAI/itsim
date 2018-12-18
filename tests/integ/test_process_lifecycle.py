5590dbf78c6bfaacc0a038f8bb7553a1d11456b0
Benoit Hamelin <benoit@benoithamelin.com>
2018-12-18 07:39:31 -0500

    Better document timings in process lifecycle test

--- tests/integ/test_process_lifecycle.py
+++ tests/integ/test_process_lifecycle.py
@@ -1,3 +1,4 @@
+import sys
 from typing import Set, Mapping
 
 import pytest
@@ -13,43 +14,89 @@ Cemetary = Set[str]
 Name2Child = Mapping[str, Process]
 
 
-def cain(context: Context, cemetary: Cemetary, child: Name2Child) -> None:
-    advance(130)
-    assert context.process is child["cain"]
-    child["abel"].kill()
-    advance(730 - 130)
-    cemetary.add("cain")
+class Character:
+
+    def __init__(self, name, year_born, year_dead):
+        super().__init__()
+        self._name = name
+        self._year_born = year_born
+        self._year_dead = year_dead
+
+    @property
+    def year_born(self):
+        return self._year_born
+
+    @property
+    def year_dead(self):
+        return self._year_dead
+
+    @property
+    def lifetime(self):
+        return self._year_dead - self._year_born
+
+    @property
+    def name(self):
+        return self._name
+
+    def life(self, context: Context, cemetary: Cemetary, child: Name2Child) -> None:
+        try:
+            assert context.process is child[self.name]
+            self._live(context, child)
+        finally:
+            cemetary.add(self.name)
 
 
-def abel(context: Context, cemetary: Cemetary, child: Name2Child) -> None:
-    assert context.process is child["abel"]
-    try:
-        advance(1000)
-    finally:
-        cemetary.add("abel")
+class Cain(Character):
+
+    def __init__(self):
+        super().__init__("Cain", 0, 730)
+
+    def _live(self, context: Context, child: Name2Child) -> None:
+        advance(Abel.YEAR_DEAD - now())
+        child["Abel"].kill()
+        advance(self.year_dead - now())
 
 
-def seth(context: Context, cemetary: Cemetary, child: Name2Child) -> None:
-    assert context.process is child["seth"]
-    advance(912)
-    cemetary.add("seth")
+class Abel(Character):
+
+    YEAR_DEAD = 130
+
+    def __init__(self):
+        super().__init__("Abel", 0, Abel.YEAR_DEAD)
+
+    def _live(self, context: Context, child: Name2Child) -> None:
+        advance(1000)  # Dude wants to live forever but...
 
 
-def humanity(context: Context, *_) -> None:
-    advance(100000)  # Will not end.
+class Seth(Character):
+
+    def __init__(self):
+        super().__init__("Seth", Abel.YEAR_DEAD, Abel.YEAR_DEAD + 912)
+
+    def _live(self, context: Context, child: Name2Child) -> None:
+        advance(self.lifetime)
+
+
+class Humanity(Character):
+
+    def __init__(self):
+        super().__init__("Humanity", 0, sys.maxsize)
+
+    def _live(self, context: Context, child: Name2Child) -> None:
+        advance(100000)  # Will not end.
 
 
 def adameve(context: Context, cemetary: Cemetary) -> None:
     child: Name2Child = {}
-    for name, moment in [(cain, 0), (abel, 0), (seth, 130), (humanity, 0)]:
-        child[name.__name__] = context.process.fork_exec_in(moment, name, cemetary, child)
+    for c in [Cain(), Abel(), Seth(), Humanity()]:
+        child[c.name] = context.process.fork_exec_in(c.year_born, c.life, cemetary, child)
 
-    for name_expected, moment in [("abel", 130), ("cain", 730), ("seth", 130 + 912)]:
-        child[name_expected].wait()
-        assert now() == pytest.approx(moment)
+    for c in [Abel(), Cain(), Seth()]:
+        child[c.name].wait()
+        assert now() == pytest.approx(c.year_dead)
 
     try:
-        child["humanity"].wait(2000)
+        child["Humanity"].wait(2000)
         pytest.fail("Humanity has yet to start killing itself with CO2.")
     except Timeout:
         pass
@@ -62,4 +109,4 @@ def test_context_process_lifecycle():
     cemetary = set()
     Endpoint().with_proc(sim, adameve, cemetary)
     sim.run(4000)
-    assert cemetary == {"adameve", "cain", "abel", "seth"}
+    assert cemetary == {"adameve", "Cain", "Abel", "Seth"}
