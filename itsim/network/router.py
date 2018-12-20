1037f687a0d963bc98047b71722852c5be3689ba
Matt <matt.craddock@elementai.com>
2018-12-20 17:28:37 -0500

    Drop unused aspects of Router

--- itsim/network/router.py
+++ itsim/network/router.py
@@ -1,37 +1,8 @@
 from itsim.machine.node import Node
-from itsim.machine.process_management.daemon import Daemon
-from itsim.machine.socket import Socket
 from itsim.network.link import Link
-from itsim.network.location import Location
 from itsim.network.packet import Packet
 from itsim.network.route import Local
 from itsim.simulator import Simulator
-from itsim.software.context import Context
-
-from typing import cast, MutableMapping
-
-
-class _PassThroughRoutingDaemon(Daemon):
-    def __init__(self):
-        super().__init__(self._route)
-        self._mac_map: MutableMapping[str, Location] = {}
-
-    def _route(self, context: Context, packet: Packet, socket: Socket):
-        if Router.MAC_FIELD not in packet.payload:
-            return
-
-        mac = str(packet.payload[Router.MAC_FIELD])
-        if Router.FINAL_DEST_FIELD in packet.payload and isinstance(packet.payload[Router.FINAL_DEST_FIELD], Location):
-            if mac not in self._mac_map:
-                self._mac_map[mac] = packet.source
-
-            dest = cast(Location, packet.payload[Router.FINAL_DEST_FIELD])
-        else:
-            if mac not in self._mac_map:
-                return
-            dest = self._mac_map[mac]
-
-        socket.send(dest, packet.byte_size, packet.payload)
 
 
 class Router(Node):
@@ -43,9 +14,6 @@ class Router(Node):
     :param wan: WAN interface, articulated by a ``itsim.network.Link`` instance.
     :param lan: LAN links connected to this router.
     """
-
-    FINAL_DEST_FIELD = "final_dest"
-    MAC_FIELD = "mac"
 
     def __init__(self, sim: Simulator, link: Link) -> None:
         super().__init__()
