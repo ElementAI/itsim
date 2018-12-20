34fd251d0c6a9e0c4c1f7ddaaa5a4292260abfc8
Matt <matt.craddock@elementai.com>
2018-12-20 17:15:12 -0500

    While: true for arbitrarily long simulation

--- examples/xmas/beaconing.py
+++ examples/xmas/beaconing.py
@@ -21,29 +21,31 @@ size_dist = normal(100 * B, 300 * B)
 @malware
 def client(context: Context, router: Router) -> None:
     with context.node.bind(Protocol.UDP) as socket:
-        socket.send(Location("10.11.12.20", 9887), next(size_dist), {"content": "ping"})
-        try:
-            packet = socket.recv(1 * S)
-        except Timeout:
-            pytest.fail("Supposed to receive the packet before timeout.")
-        assert packet.payload["content"] == "pong"
+        while True:
+            socket.send(Location("10.11.12.20", 9887), next(size_dist), {"content": "ping"})
+            try:
+                packet = socket.recv(1 * S)
+            except Timeout:
+                pytest.fail("Supposed to receive the packet before timeout.")
+            assert packet.payload["content"] == "pong"
 
-        try:
-            socket.recv(5 * S)
-            pytest.fail()
-        except Timeout:
-            assert now() > 5.0
-        finally:
-            ledger.add("client")
+            try:
+                socket.recv(5 * S)
+                pytest.fail()
+            except Timeout:
+                assert now() > 5.0
+            finally:
+                ledger.add("client")
 
 
 @malware
 def server(context: Context) -> None:
     with context.node.bind(Protocol.UDP, 9887) as socket:
-        packet = socket.recv()
-        assert packet.payload["content"] == "ping"
-        socket.send(packet.source, 8, {"content": "pong"})
-        ledger.add("server")
+        while True:
+            packet = socket.recv()
+            assert packet.payload["content"] == "ping"
+            socket.send(packet.source, 8, {"content": "pong"})
+            ledger.add("server")
 
 
 def test_packet_transfer():
@@ -59,6 +61,6 @@ def test_packet_transfer():
     ponger = Endpoint().connected_to_static(link, "10.11.12.20", [route])
     ponger.run_proc(sim, server)
 
-    sim.run(10.0 * S)
+    sim.run(600.0 * S)
 
     assert ledger == {"client", "server"}
