c552d57210195a77994a10ec6c81d5460d5e4adb
Matt <matt.craddock@elementai.com>
2019-01-15 13:55:20 -0500

    Confirm Singleton is shared across multiple classes

--- tests/test_itsim.py
+++ tests/test_itsim.py
@@ -49,3 +49,17 @@ def test_has():
     Test1()
     assert Singleton.has_instance(Test1)
     assert not Singleton.has_instance(Test2)
+
+
+def test_singleton_shared():
+    class Test1(metaclass=Singleton):
+        pass
+
+    class Test2(metaclass=Singleton):
+        pass
+
+    a = Test1()
+    b = Test2()
+
+    assert a is Singleton._instances[Test1]
+    assert b is Singleton._instances[Test2]
